29754351e7f555da025ee7ef4b9cb5d44a6c8665
trishantpahwa <trishantpahwa@gmail.com>
2021-04-04 02:56:49 +0530

    Added documentation.

--- backend_api/src/models/statistics.py
+++ backend_api/src/models/statistics.py
@@ -2,13 +2,34 @@ from time import time
 
 
 class statistics:
-
+    '''
+        %%%% statistics %%%%
+        description: Statistics of logs for each successful request, whose data was found, with recorded timestamp
+        variables:
+         - __data: list<dict<str, str>>
+        functions:
+         - search: Inserts entry into the log for the searched IFSC code.
+         - get_data: Formats the statistics data w.r.t order and limit
+    '''
     __data = []
 
     def search(self, ifsc):
+        '''
+            description: Inserts entry into the log for the searched IFSC code.
+            parameters:
+             - ifsc: (required) str
+            returns: None
+        '''
         self.__data.append({'ifsc': ifsc, 'timestamp': time()})
 
     def get_data(self, order='ASC', limit=None):
+        '''
+            description: Formats the statistics data w.r.t order and limit
+            parameters:
+             - order: (optional, default="DESC") str
+             - limit: (optional, default=10) int
+            returns: list<dict<str,str>>
+        '''
         if order == 'ASC':
             if limit is not None:
                 return self.__data[:limit]
